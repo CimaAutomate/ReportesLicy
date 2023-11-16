0e2b94da16a281bfce972dff8ed4208ff6806049
Felix Escalera <felix.escalera@serviciosti.com>
2023-11-16 16:43:19 -0600

    created new reportDatasource

--- DataSource/DatasourceModels/ImpresionesPorMunicipioFecha.cs
+++ DataSource/DatasourceModels/ImpresionesPorMunicipioFecha.cs
@@ -2,7 +2,7 @@
 {
     public class ImpresionesPorMunicipioFecha
     {
-        public string? Municipio { get; set; }
+        public string Municipio { get; set; }
         public int Impresiones { get; set; }
         public DateTime FechaDePago { get; set; }
         public DateTime FechaDeCaptura { get; set; }
