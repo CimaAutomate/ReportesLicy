989d21410bafba8887e06253de6ed618033d92de
Felix Escalera <felix.escalera@serviciosti.com>
2023-11-17 08:26:10 -0600

    created datasource for vigencias report

---
+++ DataSource/Datasources/Impresiones/ImpresionesMunicipioVigenciaFechaDatasource.cs
@@ -0,0 +1,52 @@
+﻿using DataSource.DatasourceModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSource.Datasources.Impresiones
+{
+    public class ImpresionesMunicipioVigenciaFechaDatasource
+    {
+
+        public List<ImpresionesMunicipioVigenciaFechaModel> GetData(DateTime fechaInicio, DateTime fechaFin, int municipioID, int TipoID)
+        {
+            return GetDummyData();
+        }
+
+        private static List<ImpresionesMunicipioVigenciaFechaModel> GetDummyData()
+        {
+            var listaImpresiones = new List<ImpresionesMunicipioVigenciaFechaModel>();
+
+            // Supongamos que estas son las fechas que mencionaste
+            DateTime fechaCaptrua1 = new DateTime(2023, 1, 1); // Ejemplo, ajusta según tus necesidades
+            DateTime fechaCaptrua2 = new DateTime(2023, 1, 2); // Ejemplo, ajusta según tus necesidades
+            DateTime fechaCaptrua3 = new DateTime(2023, 1, 3); // Ejemplo, ajusta según tus necesidades
+            DateTime fechaCaptrua4 = new DateTime(2023, 1, 4); // Ejemplo, ajusta según tus necesidades
+
+            DateTime fechaPago1 = new DateTime(2023, 1, 10); // Ejemplo, ajusta según tus necesidades
+            DateTime fechaPago2 = new DateTime(2023, 1, 11); // Ejemplo, ajusta según tus necesidades
+
+            // Datos para Municipio 1
+            listaImpresiones.Add(new ImpresionesMunicipioVigenciaFechaModel { Municipio = "Municipio 1", Impresiones = 100, FechaDeCaptura = fechaCaptrua1, FechaDePago = fechaPago1, Anios = 2 });
+            listaImpresiones.Add(new ImpresionesMunicipioVigenciaFechaModel { Municipio = "Municipio 1", Impresiones = 150, FechaDeCaptura = fechaCaptrua1, FechaDePago = fechaPago1, Anios = 2 });
+            listaImpresiones.Add(new ImpresionesMunicipioVigenciaFechaModel { Municipio = "Municipio 1", Impresiones = 200, FechaDeCaptura = fechaCaptrua2, FechaDePago = fechaPago2, Anios = 1 });
+            listaImpresiones.Add(new ImpresionesMunicipioVigenciaFechaModel { Municipio = "Municipio 1", Impresiones = 250, FechaDeCaptura = fechaCaptrua2, FechaDePago = fechaPago2, Anios = 3 });
+
+            // Datos para Municipio 2
+            listaImpresiones.Add(new ImpresionesMunicipioVigenciaFechaModel { Municipio = "Municipio 2", Impresiones = 300, FechaDeCaptura = fechaCaptrua1, FechaDePago = fechaPago1, Anios = 2 });
+            listaImpresiones.Add(new ImpresionesMunicipioVigenciaFechaModel { Municipio = "Municipio 2", Impresiones = 350, FechaDeCaptura = fechaCaptrua2, FechaDePago = fechaPago1, Anios = 2 });
+            listaImpresiones.Add(new ImpresionesMunicipioVigenciaFechaModel { Municipio = "Municipio 2", Impresiones = 400, FechaDeCaptura = fechaCaptrua3, FechaDePago = fechaPago2, Anios = 1 });
+            listaImpresiones.Add(new ImpresionesMunicipioVigenciaFechaModel { Municipio = "Municipio 2", Impresiones = 450, FechaDeCaptura = fechaCaptrua4, FechaDePago = fechaPago2, Anios = 3 });
+
+            // Datos para Municipio 3
+            listaImpresiones.Add(new ImpresionesMunicipioVigenciaFechaModel { Municipio = "Municipio 3", Impresiones = 500, FechaDeCaptura = fechaCaptrua1, FechaDePago = fechaPago1, Anios = 2 });
+            listaImpresiones.Add(new ImpresionesMunicipioVigenciaFechaModel { Municipio = "Municipio 3", Impresiones = 550, FechaDeCaptura = fechaCaptrua2, FechaDePago = fechaPago1, Anios = 2 });
+            listaImpresiones.Add(new ImpresionesMunicipioVigenciaFechaModel { Municipio = "Municipio 3", Impresiones = 600, FechaDeCaptura = fechaCaptrua3, FechaDePago = fechaPago2, Anios = 1 });
+            listaImpresiones.Add(new ImpresionesMunicipioVigenciaFechaModel { Municipio = "Municipio 3", Impresiones = 650, FechaDeCaptura = fechaCaptrua4, FechaDePago = fechaPago2, Anios = 3 });
+
+            return listaImpresiones;
+        }
+    }
+}
---
+++ DataSource/Repositories/VigenciasRepository.cs
@@ -0,0 +1,49 @@
+﻿using DataSource.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSource.Repositories
+{
+    public class VigenciasRepository
+    {
+        public List<Vigencia> GetVigencias()
+        {
+            return getDummy();
+        }
+
+        private List<Vigencia> getDummy()
+        {
+            return new List<Vigencia>(){
+                new Vigencia()
+                {
+                  IdVigencia = 1,
+                  Activo = true,
+                  Anios = 1,
+                  Monto = 1000,
+
+                },
+                new Vigencia()
+                {
+                  IdVigencia = 2,
+                  Activo = true,
+                  Anios = 2,
+                  Monto = 2000,
+
+                },
+                new Vigencia()
+                {
+                  IdVigencia = 3,
+                  Activo = true,
+                  Anios = 3,
+                  Monto = 3000,
+
+                },
+            }
+
+        }
+
+    }
+}
