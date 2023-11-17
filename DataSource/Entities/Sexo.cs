d4a306e36d7a16b7ea845f04ebd14fdcc815f19f
Felix Escalera <felix.escalera@serviciosti.com>
2023-11-16 19:50:55 -0600

    creee ele accesos a datos del reporte de impresiones por genero

---
+++ DataSource/Entities/Sexo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSource.Entities
+{
+    public class Sexo
+    {
+        public int IdSexo { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+
+}
